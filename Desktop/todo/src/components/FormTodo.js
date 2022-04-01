7717de8b12400b280da356bef37749126215fe1b
ismailkhan-07 <ismailkhan358391@gmail.com>
2022-04-01 01:26:41 -0700

    first push

---
+++ Desktop/todo/src/components/FormTodo.js
@@ -0,0 +1,29 @@
+import { Button, Form } from 'react-bootstrap';
+import { useState } from "react";
+
+
+
+function FormTodo({ addTodo }) {
+    const [value, setValue] = useState("");
+  
+    const handleSubmit = e => {
+      e.preventDefault();
+      if (!value) return;
+      addTodo(value);
+      setValue("");
+    };
+  
+    return (
+      <Form onSubmit={handleSubmit}> 
+      <Form.Group>
+        <Form.Label><b>Add Todo</b></Form.Label>
+        <Form.Control type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
+      </Form.Group>
+      <Button variant="primary mb-3" type="submit">
+        Submit
+      </Button>
+    </Form>
+    );
+  }
+
+  export default FormTodo
